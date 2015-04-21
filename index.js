672be39f86f87819849201fcfaaca9a537b895ec
Aaron Chambers <achambers@gmail.com>
2015-04-21 13:06:22 +0100

    Now we can create a tag from the hash of the index.html

--- index.js
+++ index.js
@@ -2,5 +2,28 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-deploy-tag'
+  name: 'ember-cli-deploy-tag',
+
+  createDeployPlugin: function(options) {
+    var tags = require('./lib/tags');
+
+    function tagFor(type) {
+      var defaultType = 'index-hash';
+      return tags[type] || tags[defaultType];
+    }
+
+    return {
+      name: options.name,
+
+      build: function(context) {
+        var config = context.config.get(this.name);
+        var data   = context.data;
+
+        var Tag = tagFor(config.type);
+        var tag = new Tag(data);
+
+        context.data.tag = tag.generate();
+      }
+    };
+  }
 };
